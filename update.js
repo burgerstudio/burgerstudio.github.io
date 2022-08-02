2c2dca5354eae63028be6d849f9c3a59b97fc7d8
Burger Studio <87225723+burgerstudio@users.noreply.github.com>
2022-08-02 11:46:05 +0800

    Add files via upload

--- update.js
+++ update.js
@@ -1,4 +1,11 @@
 function update(){
+    var version = 220802;
+    document.getElementById("updateDate").innerHTML =  "";
+    document.getElementById("updateChange").innerHTML =  "";
+	document.getElementById("updateNote").innerHTML =  "";
+	document.getElementById("updateSpinner").style = "display: none;";
+	document.getElementById("updateAvailable").style = "display: none;";
+	document.getElementById("alreadyUpdate").style = "display: none;";
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -7,10 +14,10 @@ xmlhttp.onreadystatechange = function() {
     document.getElementById("updateChange").innerHTML =  "新版变化：<br>" + myObj.change;
 	document.getElementById("updateNote").innerHTML =  myObj.note;
 	document.getElementById("updateSpinner").style = myObj.spinner;
-	if (myObj.versionCode > 220801){
+	if (myObj.versionCode > version){
     	document.getElementById("updateAvailable").style = "display: block;";
 	}
-	if (myObj.versionCode == 220801){
+	if (myObj.versionCode == version){
     	document.getElementById("alreadyUpdate").style = "display: block;";
 	}
   }
