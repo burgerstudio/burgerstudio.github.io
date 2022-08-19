99c197348c2425e2842101f2b3c43c1ec8222179
Burger Studio <87225723+burgerstudio@users.noreply.github.com>
2022-08-19 09:34:10 +0800

    Add files via upload

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 function update(){
-    var version = 220818;
+    var version = 220819;
     document.getElementById("updateDate").innerHTML =  "";
     document.getElementById("updateChange").innerHTML =  "";
 	document.getElementById("updateNote").innerHTML =  "";
