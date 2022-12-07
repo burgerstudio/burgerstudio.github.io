efe9cc352f67d5763b349af3f582b1b327a769cd
Burger Studio <87225723+burgerstudio@users.noreply.github.com>
2022-12-07 14:52:23 +0800

    Add files via upload

--- update.js
+++ update.js
@@ -1,27 +1,27 @@
+var version = 221207;
 function update(){
-    var version = 220905;
     document.getElementById("updateDate").innerHTML =  "";
     document.getElementById("updateChange").innerHTML =  "";
 	document.getElementById("updateNote").innerHTML =  "";
 	document.getElementById("updateSpinner").style = "display: block;margin-top: 25%;";
 	document.getElementById("updateAvailable").style = "display: none;";
 	document.getElementById("alreadyUpdate").style = "display: none;";
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    document.getElementById("updateDate").innerHTML =  "更新日期：" + myObj.updateDate;
-    document.getElementById("updateChange").innerHTML =  "新版变化：<br>" + myObj.change;
-	document.getElementById("updateNote").innerHTML =  myObj.note;
-	document.getElementById("updateSpinner").style = myObj.spinner;
-	if (myObj.versionCode > version){
-    	document.getElementById("updateAvailable").style.display = "block";
-	}
-	if (myObj.versionCode == version){
-    	document.getElementById("alreadyUpdate").style.display = "block";
-	}
-  }
-};
-xmlhttp.open("GET", "https://burgerstudio.github.io/version.json", true);
-xmlhttp.send();
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+  	if (this.readyState == 4 && this.status == 200) {
+    	var VersionInfo = JSON.parse(this.responseText);
+    	document.getElementById("updateDate").innerHTML =  "更新日期：" + VersionInfo.updateDate;
+    	document.getElementById("updateChange").innerHTML =  "新版变化：<br>" + VersionInfo.change;
+		document.getElementById("updateNote").innerHTML =  VersionInfo.note;
+		document.getElementById("updateSpinner").style = VersionInfo.spinner;
+		if (VersionInfo.versionCode > version){
+    		document.getElementById("updateAvailable").style.display = "block";
+		}
+		if (VersionInfo.versionCode == version){
+    		document.getElementById("alreadyUpdate").style.display = "block";
+		}
+  	}
+	};
+	xmlHttp.open("GET", "https://burgerstudio.github.io/version.json", true);
+	xmlHttp.send();
 }
